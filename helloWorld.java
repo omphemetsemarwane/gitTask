42d893e4f94b5d89358663c77bdd1546afd8b93e
omphemetse <omphemetsemarwane@gmail.com>
2021-08-11 16:11:57 +0200

    added new file newFile.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
+//this program prints out "Hello World"
 public class helloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		String name;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		name = input.nextLine();
+		System.out.println("Your name is: " + name);
+		
 		// TODO Auto-generated method stub
 
 	}
